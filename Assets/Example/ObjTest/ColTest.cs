9ba60fb38a5d6ce893f55d84c55506c218546feb
Mark <mark@u3dc.com>
2017-06-20 19:45:25 +0800

    Add new test scene

---
+++ Assets/Example/ObjTest/ColTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniEventDispatcher;
+
+public class ColTest : MonoBehaviour {
+
+	void OnCollisionEnter(Collision collision)
+	{
+		NotificationCenter.Get ().DispatchEvent ("ColEvt",this.gameObject);
+		Debug.Log ("Dispatch Event");
+	}
+
+	void OnDestroy()
+	{
+		NotificationCenter.Get ().RemoveEventListener ("ColEvt");
+		Debug.Log ("Remove Event");
+	}
+
+}
---
+++ Assets/Example/ObjTest/PlaneCol.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniEventDispatcher;
+
+public class PlaneCol : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		NotificationCenter.Get ().AddEventListener ("ColEvt", OnColEvt);
+		Debug.Log ("Regist Event");
+	}
+	
+
+	void OnColEvt(Notification notf)
+	{
+		GameObject.Destroy ((GameObject)notf.param);
+	}
+
+}
